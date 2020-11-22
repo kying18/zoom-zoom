ba3f7b955b53eb910939503bd5d31537d609dfb0
Mark Olchanyi <olchanyi@mit.edu>
2020-11-22 14:00:32 -0500

    modified L2 loss

--- julia_code/pinn.jl
+++ julia_code/pinn.jl
@@ -8,7 +8,7 @@ Pkg.activate(".")
 # Pkg.add("Plots")
 # Pkg.add("Statistics")
 # Pkg.add("DiffEqSensitivity")
-using DiffEqFlux, Flux, Optim, OrdinaryDiffEq, Plots, Statistics, DiffEqSensitivity
+using DiffEqFlux, Flux, Optim, OrdinaryDiffEq, Plots, Statistics, DiffEqSensitivity,StaticArrays
 
 # 21 inputs for each input item in our x vector, u vector, p vector
 # 8 outputs for each output item in the newly predicted y vector
@@ -23,13 +23,48 @@ nn = Chain(
     Dense(128, 8),
 )
 
-loss() = 0 # TODO replace this
+
+#define states in vect with dims as:
+# u =
+    #1| X    - x pos
+    #2| Y    - y pos
+    #3| phi  - steering angle
+    #4| v_x  - x vel (w.r.t. body)
+    #5| v_y  - y vel (w.r.t. body)
+    #6| r    - yaw rate
+    #7| del  - steering angle
+    #8| T    - driver command
 
 ############ L2 Loss based on state params #############
-function loss_L2()
+function loss_L2(u)
+l_r = 2 # set rear axel length
+l_f = 2 # set front axel length
 
+I_z = 2 # set moment of inertia
+contr_gain = 1 # TODO define controller gain
+
+r_targ = u[7]*(u[4]/(l_f + l_r))
+tau_tv = (r_targ - u[6])*contr_gain
+
+# define bicycle model ODE
+u_der = @MVector[0.0 for i = 1:8]
+
+u_der[1] = (u[4]*cos(u[3]))-(u[5]*sin(u[3]))
+u_der[2] = (u[4]*cos(u[3]))-(u[5]*sin(u[3]))
+u_der[3] = r
+u_der[4] = (m^(-1))*(F_x - F_ry*sin(u[7]) + m*u[5]*u[6])
+u_der[5] = (m^(-1))*(F_ry + F_ry*cos(u[7]) - m*u[4]*u[6])
+u_der[6] = (I_z^(-1))*(F_Fy*l_f*cos(u[7]) - F_ry*l_r - m*u[4]*u[6])
+u_der[7] = del_del # TODO define change in del
+u_der[8] = del_T # TODO define change in T
+
+solve_RK(u_der, init_params) # TODO implement RK solver for this 
 
 end
+
+
+
+
 
 opt = Flux.Descent(0.01)
 data = Iterators.repeated((), 5000)
@@ -37,9 +72,9 @@ iter = 0
 cb = function () #callback function to observe training
   global iter += 1
   if iter % 500 == 0
-    display(loss())
+    display(loss_L2())
   end
 end
-display(loss())
+display(loss_L2())
 
-Flux.train!(loss, Flux.params(nn), data, opt; cb=cb)
+Flux.train!(loss_L2, Flux.params(nn), data, opt; cb=cb)
