36c6dbcd29b225fd2eb497e4526b7e7b82cbd77c
ewuirl <emwu@mit.edu>
2020-11-24 00:54:48 -0500

    Updated data generator to add optional arguments for adding noise, and for modifying and collecting the parameters. Also edited the data generator to update each x variable separately (it looks like every x variable was being identically updated. Now the test case at the bottom doesn't work.

--- julia_code/bicycle_model.jl
+++ julia_code/bicycle_model.jl
@@ -1,8 +1,10 @@
 cd(@__DIR__)
 using Pkg
 Pkg.activate(".")
+using Random
+using Distributions
 
-function tire_force(alpha, Fz, p) 
+function tire_force(alpha, Fz, p)
     -alpha .* p[6] .* Fz/p[7]
 end
 function normal_force(x, u, p)
@@ -33,7 +35,7 @@ function bicycle_model(x, u, p)
     # x, y, psi, vx, vy, r, steer, T = x
     # above is from the paper, but let's use the matlab code instead
     x, y, psi, vx, vy, r, theta, steer = x
-    v_theta, D, delta = u  # velocity along path, accel command, commanded steer rate 
+    v_theta, D, delta = u  # velocity along path, accel command, commanded steer rate
 
     # compute slip angles
     alpha_f = atan((vy + r * lf) / vx) + steer
@@ -46,7 +48,7 @@ function bicycle_model(x, u, p)
 
     # torque to force
     F_net = m * D
-    
+
     # torque vectoring
     F_xf = lf / l * F_net
     F_xr = lr / l * F_net
@@ -70,26 +72,135 @@ function bicycle_model(x, u, p)
     return dx
 end
 
-function gen_data(x0, g, p, collect_at=0:1:10)
+"""
+    percent_noise_flat(x, percent_noise)
+
+Computes noise by adding or subtracting a specified percentage of the provided
+x array as error: noisy_x = x +/- (percent)*x
+"""
+function percent_noise_flat(x, percent_noise)
+    # Create array of +/-1 the size of x
+    noise = rand([Float32(-1.0),Float32(1.0)],length(x))
+    # Multiply the percent noise by the x values, and then multiply by the
+    # +/- noise array. Add to the x values.
+    noisy_x = x + percent_noise * x .* noise
+    return noisy_x
+end
+
+"""
+    percent_noise_normal(x, percent_noise)
+
+Adds normally distributed noise by calculating noise N(0.0, percent_noise) and
+computing noisy_x = x + N(0.0, percent_noise)*x.
+"""
+function percent_noise_normal(x, percent_noise)
+    # Create an array the size of x
+    noisy_x = Vector{Float32}(undef,length(x))
+    # For each x, sample from the N(x, percent*x) distribution
+    for i in 1:length(x)
+        noisy_x[i] = x[i] * (Float32(1.0) + rand(Normal(0.0,percent_noise)))
+    end
+    return noisy_x
+end
+
+test_xs[1,:]
+percent_noise_normal(test_xs[1,:],0.05)
+"""
+    change_params_percent_normal(params, i, percent_dp)
+
+    Takes params, an array of parameters, an index i, and a floating point
+    percentage, percent_dp. For each of the parameters p, it computes a normally
+    distributed change dp = N(0.0,percent_dp*p), and decides the sign of the
+    change based on the sign of the previous dp. There is a 10% chance that the
+    sign of dp differs from the previous dp.
+"""
+function change_params_percent_normal(params, i, percent_dp)
+    # Create a vector to store the new parameters in
+    new_params = Vector{Float32}(undef,10)
+    # Calculate the previous change in parameters to figure out its sign later
+    if i > 2
+        prev_dp = params[i-1,:] - params[i-2,:]
+    else
+        # Set prev_dp to an array of -1.0s for the first parameter change
+        prev_dp = -1.0 * ones(Float32,length(params[i-1,:]))
+    end
+    # Iterate through the parametrs and calculate the next parameter
+    for j in 1:length(params[i-1,:])
+        # Pick a new parameter
+        dp = abs(rand(Normal(0.0, percent_dp[j]*params[i-1,j])))
+        # Generate a random number [0,1) to decide the sign of dp
+        change_sign = rand(Float64)
+        # Figure out the sign of the previous dp
+        if prev_dp[j] < 0.0
+            sign = -1.0
+        else
+            sign = 1.0
+        end
+        # 10% chance of changing the sign of dp from that of the previous dp
+        if change_sign <= 0.1
+            sign = sign*-1.0
+        end
+        # Add dp to the previous parameters
+        new_params[j] = params[i-1,j] + sign * dp
+    end
+    return new_params
+end
+
+function gen_data(x0, g, p, collect_at=0:1:10; gen_params=false,
+    percent_dp=1e-4*ones(Float32,10), add_noise=false, percent_noise=0.01)
+    # Figure out number of data points to collect
     len=length(collect_at)
-    xs=Array{Float32,2}(undef, len,8)
+    # Array to store the state data in
+    xs=Array{Float32,2}(undef,len,8)
+    # Save the initial state
     xs[1,:]=x0
+    # Arrays to store dxs and inputs in
     dxs=Array{Float32,2}(undef, len,8)
     inputs=Array{Float32,2}(undef, len,3)
 
-    for itr in 2:len
-        inputs[itr-1,:]=g(collect_at[itr-1])
-        dxs[itr,:]=bicycle_model(xs[itr-1,:], inputs[itr-1,:], p)
-        xs[itr,:].=xs[itr-1].+dxs[itr]
+    # Create storage array if parameters are being modified
+    if gen_params != false
+        # Create an array to store parameters in
+        params=Array{Float32,2}(undef,len, length(p))
+        # Save the initial parameters
+        params[1,:] = p
     end
 
-    return [collect_at, xs, dxs, inputs]
+    # Generate the data
+    for itr in 2:len
+        # Generate inputs
+        inputs[itr-1,:]=g(collect_at[itr-1])
 
+        # If parameters are to be changed,
+        if gen_params != false
+            # Update p
+            p = gen_params(params, itr, percent_dp)
+            # Save to params array
+            params[itr,:] = p
+        end
+
+        # Calculate dx
+        dxs[itr,:]=bicycle_model(xs[itr-1,:], inputs[itr-1,:], p)
+        # Compute next x
+        # xs[itr,:].=xs[itr-1].+dxs[itr]
+        xs[itr,:]=xs[itr-1,:].+dxs[itr,:]
+
+        # If noise is to be added
+        if add_noise != false
+            # Compute next x with noise
+            xs[itr,:] = add_noise(xs[itr,:], percent_noise)
+        end
+    end
+
+    if gen_params == false
+        return [collect_at, xs, dxs, inputs]
+    else
+        return [collect_at, xs, dxs, inputs, params]
+    end
 end
 
-
 function example_g(t)
-    
+
     [sin(t), t, cos(t)]
 
 end
@@ -97,6 +208,10 @@ end
 x0 = [1,2,1,3,1,1,1,1]
 p = [1292.2, 3,1.006,1.534,0.5,8,0.5,0.1,0.2,0.1]
 data = gen_data(x0,example_g,p)
+normal_noisy_data = gen_data(x0,example_g,p,add_noise=percent_noise_normal)
+normal_noisy_data = gen_data(x0,example_g,p,add_noise=percent_noise_flat)
+normal_noisy_data = gen_data(x0,example_g,p,gen_params=change_params_percent_normal)
+normal_noisy_data = gen_data(x0,example_g,p,add_noise=percent_noise_normal,gen_params=change_params_percent_normal)
 
 using Plots
 plot(data[1],data[2][:,1])
