61959b3e3a91439a3648a2af86186fb8ab029e32
markolchanyi <olchanyi@mit.edu>
2020-12-17 19:56:09 -0500

    added new steer and moving average

--- julia_code/pack_race_data.jl
+++ julia_code/pack_race_data.jl
@@ -9,7 +9,7 @@ using MAT, NumericalIntegration,ForwardDiff,FDM,FiniteDifferences
 function unpack_MATs(mat_overpath="../roborace_data/SpeedGoat.mat")
     header = matopen(mat_overpath)
 
-    # I was too lazy to 
+    # I was too lazy to
     # what do i do cornering stiffness measurements ...
     x_block = hcat(read(header, "OXT_E")[7356:13052,1],read(header, "OXT_E")[7356:13052,2]) # ``varname`` into scope
     y_block = hcat(read(header, "OXT_N")[7356:13052,1],read(header, "OXT_N")[7356:13052,2])
@@ -17,12 +17,13 @@ function unpack_MATs(mat_overpath="../roborace_data/SpeedGoat.mat")
     vy_block = hcat(read(header, "OXT_vyCG")[7356:13052,1],read(header, "OXT_vyCG")[7356:13052,2])
     psi_block = hcat(read(header, "MAP_psiDes")[1:10:end,1][7356:13052],read(header, "MAP_psiDes")[1:10:end,2][7356:13052])
     psi_block_full = read(header, "MAP_psiDes")
-    r_block = hcat(psi_block[:,1],centraldiff(psi_block_full)[1:10:end][7356:13052])
+    r_block = hcat(psi_block[:,1],moving_avg(centraldiff(psi_block_full),50)[1:10:end][7356:13052])
 
 
     D_block = hcat(read(header, "CMD_gLongRequest")[1:10:end,1][7356:13052],read(header, "CMD_gLongRequest")[1:10:end,2][7356:13052])
-    steer_block = hcat(psi_block[:,1],atan.(2*tan.(atan.(vy_block[:,2]./vx_block[:,2])-psi_block[:,2])))
-    Delta_block = hcat(psi_block[:,1],centraldiff(steer_block))
+    #steer_block = hcat(psi_block[:,1],atan.(2*tan.(atan.(vy_block[:,2]./vx_block[:,2])-psi_block[:,2])))
+    steer_block = hcat(read(header, "DCU_aWheelFMean")[1:10:end,1][7356:13052],read(header, "DCU_aWheelFMean")[1:10:end,2][7356:13052])
+    Delta_block = hcat(psi_block[:,1],moving_avg(centraldiff(steer_block),50))
 
     #(1)x, (2)y, (3)psi, (4)vx, (5)vy, (6)r, (7)steer, (8)D, (9) Delta
     #                                                ^^comm(1), comm(2)
@@ -33,6 +34,8 @@ function unpack_MATs(mat_overpath="../roborace_data/SpeedGoat.mat")
     return u_block
 end
 
+
+# how to call it
 gg = unpack_MATs()
 
 
@@ -50,3 +53,20 @@ end
 if_on = hcat(read(header, "MAP_UxDes")[1:10:end,1],read(header, "MAP_UxDes")[1:10:end,2])
 
 if_bool = filter(x->x>0.0, if_on[:,2])
+
+
+
+#moving_average(vs,n) = [sum(@view vs[i:(i+n-1)])/n for i in 1:(length(vs)-(n-1))]
+
+function moving_avg(X::Vector,numofele::Int)
+    BackDelta = div(numofele,2)
+    ForwardDelta = isodd(numofele) ? div(numofele,2) : div(numofele,2) - 1
+    len = length(X)
+    Y = similar(X)
+    for n = 1:len
+        lo = max(1,n - BackDelta)
+        hi = min(len,n + ForwardDelta)
+        Y[n] = mean(X[lo:hi])
+    end
+    return Y
+end
